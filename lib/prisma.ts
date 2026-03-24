235227530151e30aafe231ee30c1ecc169671737
Serqx <serqx0662@gmail.com>
2026-03-24 12:36:39 +0600

    fix prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,21 +1,17 @@
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"; // Исправлен путь импорта
 
 const connectionString = `${process.env.DATABASE_URL}`;
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
 
-const prismaClientSingleton = () => {
-    return new PrismaClient({ adapter });
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-declare global {
-    var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-}
+export const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient({
+        adapter: adapter,
+    });
 
-const prisma = globalThis.prisma ?? prismaClientSingleton();
-
-export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
