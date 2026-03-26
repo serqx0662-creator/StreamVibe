1dc83a9e741cf09d8b5f3a56b66b17bbb1094fb3
Serqx <serqx0662@gmail.com>
2026-03-26 09:50:35 +0600

    removed Prism and Next Auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
-import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 
 export default async function RootPage() {
-    const session = await auth();
-
-    if (!session) {
-        redirect("/login");
-    } else {
-        redirect("/register");
-    }
-
-    return null;
+    redirect("/Home");
 }
