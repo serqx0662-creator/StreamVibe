6030c1b2c08a4fe37440a9b9cccb15ac5efff72e
Serqx <serqx0662@gmail.com>
2026-03-26 10:02:03 +0600

    remove prisma and next-auth for cleaner build

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,14 @@
-import { withNextVideo } from "next-video/process";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Если используешь изображения из TMDB, добавь это:
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'image.tmdb.org',
-        pathname: '/t/p/**',
       },
     ],
   },
-  // --- ДОБАВЬ ЭТИ СЕКЦИИ НИЖЕ ---
-  typescript: {
-    // Разрешает завершить билд, даже если в проекте есть ошибки типов
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Разрешает завершить билд, даже если линтер нашел ошибки (например, использование 'any')
-    ignoreDuringBuilds: true,
-  },
-  // ------------------------------
 };
 
-export default withNextVideo(nextConfig, { folder: 'v' });
+export default nextConfig;
